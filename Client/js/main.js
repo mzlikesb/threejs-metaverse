e87f7fd143f70c5f98a042b5f63aaaccf3f06aaa
DY Park <mintzzzzz42@gmail.com>
2024-07-22 16:34:39 +0900

    move folder

--- Client/js/main.js
+++ Client/js/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-//import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -9,7 +9,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-//const orbit = new OrbitControls(camera, render.domElement);
+const orbit = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -17,7 +17,7 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
-//orbit.update();
+orbit.update();
 
 function animate() {
     requestAnimationFrame(animate);
